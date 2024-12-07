167f70bd28582f474ac2861f6dc115e71abfc17a
Muhammad Putra Age <muhammadputraage@gmail.com>
2024-12-07 08:28:39 +0800

    refactor navbar

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -2,8 +2,8 @@ import ROUTES from "@/constants/routes";
 import Image from "next/image";
 import Link from "next/link";
 import { Suspense } from "react";
-import SearchFurnitures from "../common/SearchFurnitures";
-import Navlinks from "./Navlinks";
+import SearchFurnitures from "./SearchFurnitures";
+import { NAV_LINKS } from "@/constants";
 
 export default function Navbar() {
   return (
@@ -12,7 +12,15 @@ export default function Navbar() {
         <Image src="/logo.svg" alt="Adinda Karya Logo" width={56} height={56} />
       </Link>
 
-      <Navlinks />
+      <ul className="flex items-center gap-8">
+        {NAV_LINKS.map((navLink) => (
+          <li key={navLink.url}>
+            <Link href={navLink.url} className="text-base font-medium">
+              {navLink.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
 
       <div className="flex items-center gap-6">
         <Suspense>
---
+++ constants/index.ts
@@ -0,0 +1,24 @@
+import ROUTES from "./routes";
+
+export const NAV_LINKS = [
+  {
+    title: "Home",
+    url: ROUTES.HOME,
+  },
+  {
+    title: "Shop",
+    url: ROUTES.SHOP,
+  },
+  {
+    title: "Blog",
+    url: ROUTES.BLOG,
+  },
+  {
+    title: "About",
+    url: ROUTES.ABOUT,
+  },
+  {
+    title: "FAQ",
+    url: ROUTES.FAQ,
+  },
+];
